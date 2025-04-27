19372acad62d36ed9ba9d00ec9eb97ed3b75888b
User <mmgeight@gmail.com>
2025-04-27 14:34:58 -0700

    Add tests for undefined PSR bits

--- psr/psr2.asm
+++ psr/psr2.asm
@@ -20,11 +20,54 @@ main:
         str     r0, [r3, REG_IME]
         mov     r1, MEM_EWRAM
 
-        ; start out in ABT mode
-        mov     r2, 0x17
+        ; enter SYS mode
+        mov     r2, 0x1F
         msr     cpsr_c, r2
 
 t001:
+        ; test reading/writing extension field (should always be zero)
+        mov     r3, 0xff
+        lsl     r3, 8
+        mov     r2, 0x42
+        lsl     r2, 8
+        msr     cpsr_x, r2
+        mrs     r1, cpsr
+        and     r1, r3
+        cmp     r1, r2
+        beq     f001
+t002:
+        cmp     r1, 0
+        bne     f002
+
+t003:
+        ; test reading/writing status field (should always be zero)
+        lsl     r2, 8
+        lsl     r3, 8
+        msr     cpsr_s, r2
+        mrs     r1, cpsr
+        and     r1, r3
+        cmp     r1, r2
+        beq     f003
+t004:
+        cmp     r1, 0
+        bne     f004
+
+t005:
+        ; test reading/writing undocumented CPSR flag bits (should always be zero)
+        lsl     r2, 8
+        lsl     r3, 8
+        msr     cpsr_f, r2
+        mrs     r1, cpsr
+        and     r1, r3
+        cmp     r1, r2
+        beq     f005
+t006:
+        lsr     r3, 4
+        and     r1, r3
+        cmp     r1, 0
+        bne     f006
+
+t007:
         ; test that bit 4 of CPSR mode is forced to 1,
         ; even if that mode value is nonsense
         mov     r2, 0x0c
@@ -32,24 +75,24 @@ t001:
         mrs     r1, cpsr
         and     r1, 0xFF
         cmp     r1, 0x1c
-        bne     f001
+        bne     f007
 
-        ; re-enter ABT mode
+        ; enter ABT mode
         mov     r2, 0x17
         msr     cpsr_c, r2
 
-t002:
+t008:
         ; test whether bit 4 of SPSR mode is forced to 1
         mov     r2, 0x03
         dw      0xE161F002  ; msr     spsr_c, r2
         mrs     r1, spsr
         and     r1, 0xFF
         cmp     r1, 0x13
-        bne     f002
+        bne     f008
 
-        ; enter SYS mode
-        mov     r1, 0x1F
-        msr     cpsr_c, r1
+        ; re-enter SYS mode
+        mov     r2, 0x1F
+        msr     cpsr_c, r2
 
         bl      eval
 
@@ -63,6 +106,42 @@ f002:
         mov     r1, 0x1F
         msr     cpsr_c, r1
         mov     r12, 2
+        bl      eval
+
+f003:
+        mov     r1, 0x1F
+        msr     cpsr_c, r1
+        mov     r12, 3
+        bl      eval
+
+f004:
+        mov     r1, 0x1F
+        msr     cpsr_c, r1
+        mov     r12, 4
+        bl      eval
+
+f005:
+        mov     r1, 0x1F
+        msr     cpsr_c, r1
+        mov     r12, 5
+        bl      eval
+
+f006:
+        mov     r1, 0x1F
+        msr     cpsr_c, r1
+        mov     r12, 6
+        bl      eval
+
+f007:
+        mov     r1, 0x1F
+        msr     cpsr_c, r1
+        mov     r12, 7
+        bl      eval
+
+f008:
+        mov     r1, 0x1F
+        msr     cpsr_c, r1
+        mov     r12, 8
         bl      eval
 
 eval:
