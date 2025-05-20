0f117277493b64fba16ff75b1185a828e99204f5
User <mmgeight@gmail.com>
2025-05-19 18:32:41 -0700

    Update comments in branches.asm

--- arm/branches.asm
+++ arm/branches.asm
@@ -85,22 +85,19 @@ t060:
         mov     r12, 60
         mov     r1, 42
         adr     r0, f060
-        ; note: I had to come up with my own syntax for this operation,
-        ; and decided to use the mnemonic "mvx rd, rm",
-        ; with "mvx" standing for "move and exchange"
-        dw      0xE12F1F10  ; mvx r1, r0
+        dw      0xE12F1F10  ; bx r1, r0
         cmp     r1, r0
         beq     t061
 f060:
         m_exit  60
 
 t061:
-        ; test whether MVX can switch into Thumb mode
+        ; test whether BX with rd != r15 can switch into Thumb mode
         mov     r12, 61
         mov     r1, 42
         adr     r0, t061a + 1
         adr     r2, t062
-        dw      0xE12F1F10  ; mvx r1, r0  ; switches to Thumb mode
+        dw      0xE12F1F10  ; bx r1, r0  ; switches to Thumb mode
         nop     ; will be executed in ARM mode, since mode is checked at decode stage
 code16
 align 2
@@ -115,7 +112,7 @@ f061:
 code32
 align 4
 t062:
-        ; check value in r1 after MVX with mode switch
+        ; check value in r1 after mode switch
         mov     r12, 62
         cmp     r1, r0
         beq     t063
